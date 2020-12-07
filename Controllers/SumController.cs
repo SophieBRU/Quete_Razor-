7aed673ec213e9e77f11bafa71fd23d04ff9c9a6
Sophie BRULTET <sophie.brultet@gmail.com>
2020-12-07 11:02:35 +0100

    Quete Razor symbole@ et bonnes pratiques

---
+++ Controllers/SumController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Quete_Razor_.Controllers
+{
+    public class SumController : Controller
+    {
+        public IActionResult Index()
+        {
+            String conclusion = String.Empty;
+            IEnumerable<Int32> integers = new Int32[] { 1, 2, 3, 4 };
+            Int32 sum = integers.Sum();
+
+            if (sum > 10000000)
+            {
+                conclusion = "You earn too much money";
+            }
+            else
+            {
+                conclusion = "You should ask for a salary raise";
+            }
+
+            return View("Index", conclusion);
+        }
+    }
+}
